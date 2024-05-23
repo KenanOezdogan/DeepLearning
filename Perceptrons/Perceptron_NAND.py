cb5750e723ff9fcb7ca647a0215c3542faf857b2
Kenan Özdogan <75647691+KenanOezdogan@users.noreply.github.com>
2024-05-23 20:26:24 +0200

    changed files
    23052024_20:26

---
+++ Perceptrons/Perceptron_NAND.py
@@ -0,0 +1,51 @@
+import numpy as np
+#Implementing a NAND Perceptron function
+def perceptron(inputs,weights,treshold):
+    result_and = And_Function(inputs,weights,treshold)
+    result_perceptron = NOT_Function(result_and)
+    
+    return result_perceptron
+
+def threshold_function(input,treshold):
+    if (input >= treshold):
+        result = 1
+    else:
+        result = 0
+        
+    return result
+    
+def NOT_Function(result_and):
+    if (result_and == 1):
+        result_not = 0
+    else: 
+        result_not = 1
+    return result_not
+
+def And_Function(inputs,weights,treshold):
+    # weighted multiply
+    summarray = np.sum(np.multiply(inputs,weights))
+    # and function
+    result_and = threshold_function(summarray,treshold)
+    return result_and
+
+print("How many entries do you have? ? \n")
+N = int(input("Number of input values ​​x: "))
+inputs = [0]*N
+weights = [0]*N
+treshold = 1.5
+
+for i in range(len(inputs)):
+    x = int(input("Value"+str(i)+" input: "))
+    inputs[i]=x
+
+print("----------------------------")
+
+for i in range(len(weights)):
+    x = int(input("Weight"+str(i)+" input: "))
+    weights[i]=x
+
+print("----------------------------")
+
+result = perceptron(inputs,weights,treshold)
+print("Logical NAND with perceptron: " + str(result))
+
--- Perceptrons/Perceptron_XOR.py
+++ Perceptrons/Perceptron_XOR.py
@@ -1,8 +1,19 @@
 import numpy as np
-#Implementing a NAND Perceptron function
+#Implementing a XOR Perceptron function, with treshold -2 in second step and 1.5 in first step
 def perceptron(inputs,weights,treshold):
-    result_and = And_Function(inputs,weights,treshold)
-    result_perceptron = NOT_Function(result_and)
+    result_first_step = Unit_Function(inputs,weights,treshold)
+    
+    treshold = 0.5
+    new_weight = -2
+
+    inputs.append(result_first_step)
+    weights.append(new_weight);
+    
+    #print(result_first_step)
+    #print(inputs)
+    
+    result_second_step = Unit_Function(inputs,weights,treshold)
+    result_perceptron = result_second_step
     
     return result_perceptron
 
@@ -11,26 +22,32 @@ def threshold_function(input,treshold):
         result = 1
     else:
         result = 0
-    
+        
+    return result
+
+"""
 def NOT_Function(result_and):
     if (result_and == 1):
         result_not = 0
     else: 
         result_not = 1
-    return result_not
-
-def And_Function(inputs,weights,treshold):
+    return result_not 
+"""
+def Unit_Function(inputs,weights,treshold):
     # weighted multiply
-    summarray = np.sum(np.multiply(inputs,weights))
-    # and function
-    result_and = threshold_function(summarray,treshold)
-    return result_and
+    summarray = np.multiply(inputs,weights)
+    summarray = np.sum(summarray)
+    
+    result_threshold = threshold_function(summarray,treshold)
+    
+    return result_threshold
 
 print("How many entries do you have? ? \n")
 N = int(input("Number of input values ​​x: "))
 inputs = [0]*N
 weights = [0]*N
 treshold = 1.5
+
 
 for i in range(len(inputs)):
     x = int(input("Value"+str(i)+" input: "))
